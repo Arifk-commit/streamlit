19ef573a4cd39b562b5f287011685eacec8e04c7
Khan Arif <khanarifshamshul@gmail.com>
2025-04-29 20:48:16 +0530

    Update age.py

--- age.py
+++ age.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import datetime
 
 st.title("Know Your Age")
-st.date_input("Enter Your Age")
+dob = st.date_input("Enter Date of Birth" ,
+              min_value=datetime.date(1800,1,1) ,
+                max_value=datetime.date.today()
+             )
+
+st.write(f"selected DOB is {dob} ")
